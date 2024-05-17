25bac67374e5173282df5a8a1cda605af6f27b46
AfolabiOmobolanle <afolabiomobolanle00@gmail.com>
2024-05-17 14:10:46 +0100

    commit

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-const express = require("express");
+
 const userRoute = require("./routes/UserRoute")
 const adminRoute = require("./routes/AdminRoute")
+
+const dotenv = require("dotenv").config({path:".env"});
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
 
 
 const app = express();
@@ -9,8 +15,9 @@ app.use(express.json());
 app.use('/user',userRoute);
 app.use('/admin',adminRoute);
 
-let PORT = process.env.PORT || 4000;
+ let PORT = process.env.PORT || 2000;
+
 
 app.listen(PORT,()=>{
-    console.log("Server is up amd running")
+    console.log(`Server is up amd running on port ${PORT}`)
 })
